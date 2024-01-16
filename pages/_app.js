1c1bc690c1eb276dd6ab4fd3db2b328668e6d48c
Andrea Hohnsen <a.hohnsen@mail.de>
2024-01-16 11:19:12 +0100

    Create animal state

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,18 @@
+import { useState } from "react";
+
 import GlobalStyle from "../styles";
 import Layout from "../components/Layout";
 
+const initialAnimals = [
+  { id: "1", name: "Cats", count: 0 },
+  { id: "2", name: "Dogs", count: 0 },
+  { id: "3", name: "Sheep", count: 0 },
+  { id: "4", name: "Dragons", count: 0 },
+];
+
 export default function App({ Component, pageProps }) {
+  const [animals, setAnimals] = useState(initialAnimals);
+
   return (
     <>
       <GlobalStyle />
